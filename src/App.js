00d06217bfcca4ceb88ae23293e440b4b1049a14
@abdurrahman0803 <abdurrahman0803@gmail.com>
2021-12-16 18:29:47 +0100

    added H1 component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
+import H1 from "./components/H1";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
-      <Header/>
+      <Header />
+      <div className="body">
+        <H1 text="I'm Abdurrahman Sogbesan" />
+        <p>
+          Frontend developer, UX architect and Javascript engineer, Photochromic
+          Lens enthusiast, practicing minimalist, and Pop Music & Jazz lover in
+          search of flow. <br /> <br /> Working with my hands to make magic
+          happen on the internet. View my Projects, Articles, Resumé, Contact
+          Me, or send me an email at hellocodewonders@gmail.com. <br /> <br />
+          See More About Me
+        </p>
+      </div>
     </div>
   );
 }
---
+++ src/components/H1.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./H1.css";
+
+function H1({ text }) {
+  return <h1>{text}</h1>;
+}
+
+export default H1;
