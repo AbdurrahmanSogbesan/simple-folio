85cc69563b60e95339782c34aac6af0d09f431b2
@abdurrahman0803 <abdurrahman0803@gmail.com>
2021-12-17 11:51:43 +0100

    added folders for each component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
-import BaseText from "./components/BaseText";
-import H1 from "./components/H1";
-import Header from "./components/Header";
-import Label from "./components/Label";
+import BaseText from "./components/BaseText/BaseText";
+import H1 from "./components/H1/H1";
+import Header from "./components/Header/Header";
+import Label from "./components/Label/Label";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <span>
           <BaseText text="Frontend developer, UX architect and Javascript engineer," />{" "}
           <Label text="Photochromic Lens" />{" "}
-          <BaseText text="enthusiast, practicing minimalist, and " /> <br/>
+          <BaseText text="enthusiast, practicing minimalist, and " /> <br />
           <Label text="Pop Music & Jazz lover" />
           <BaseText text=" in search of flow." />
         </span>
