3203b4ec607b7ed6646252aa14d58a0891dac5fe
@abdurrahman0803 <abdurrahman0803@gmail.com>
2022-01-04 12:51:25 +0100

    added h2 component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,7 +4,6 @@ import Footer from "../../components/Footer";
 import H1 from "../../components/H1";
 import Header from "../../components/Header";
 import Label from "../../components/Label";
-import ProjectCard from "../../components/ProjectCard";
 import "./Home.css";
 
 function Home() {
@@ -14,9 +13,9 @@ function Home() {
       <div className="body">
         <H1 text="I'm Abdurrahman Sogbesan" />
         <span>
-          <BaseText text="Frontend developer, UX architect and Javascript engineer," />
+          <BaseText text="Frontend developer, UX architect and Javascript engineer, " />
           <Label text="Photochromic Lens" />
-          <BaseText text="enthusiast, practicing minimalist, and " />
+          <BaseText text=" enthusiast, practicing minimalist, and " />
           <Label text="Pop Music & Jazz lover" />
           <BaseText text=" in search of flow." />
         </span>
@@ -33,10 +32,6 @@ function Home() {
           <BaseText text=", or send me an email at " />
           <Label text="abdurrahman0803@gmail.com" />
           <BaseText text="." />
-          <ProjectCard
-            title="Piggment"
-            labels={["React", "Sass & CSS", "Javascript", "Context"]}
-          />
         </span>
         <Footer linkText="See More About Me" />
       </div>
