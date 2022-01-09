b57928caddecbaf9485284dff7098962f5a50a42
@abdurrahman0803 <abdurrahman0803@gmail.com>
2022-01-09 20:07:22 +0100

    changed default value of selectedFilter state

--- src/components/ProjectFilter/index.js
+++ src/components/ProjectFilter/index.js
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import Label from "../Label";
 import "./ProjectFilter.css";
 
-function ProjectFilter({filters}) {
-  const [selectedFilter, setSelectedFilter] = useState([]);
+function ProjectFilter({ filters }) {
+  const [selectedFilter, setSelectedFilter] = useState("");
   return (
     <div>
       {filters.map((filter) => (
